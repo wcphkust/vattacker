3a18afde61b6ae627b57632f59434c7f64e46d5c
vemakereporter <vemakereporter@gmail.com>
2019-11-03 15:11:00 +0800

    add pos_tagging

--- src/Engine/Tagger.py
+++ src/Engine/Tagger.py
@@ -1,5 +1,6 @@
 import nltk
 
+
 class Tagger(object):
     """
     Tag the text
@@ -22,5 +23,15 @@ class Tagger(object):
         return
 
     def punctuation_tagging(self):
-        # TODO
-        return
+        if self.punctuation_tag is None:
+            if self.pos_tag is None:
+                self.pos_tagging()
+            self.punctuation_tag = []
+            for (token, tag) in self.pos_tag:
+                if tag != '.':
+                    self.punctuation_tag.append((token, "word"))
+                elif token == "!":
+                    self.punctuation_tag.append((token, "am punc"))
+                elif token == "?":
+                    self.punctuation_tag.append((token, "ne punc"))
+        return self.punctuation_tag
