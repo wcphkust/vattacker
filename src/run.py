ffa16a41d6939923111465684b8ba3be1e614127
vemakereporter <vemakereporter@gmail.com>
2019-11-15 16:56:08 +0800

    debug

--- src/run.py
+++ src/run.py
@@ -12,5 +12,5 @@ if __name__ == '__main__':
     if ("-single" in sys.argv):
         single_test = SingleTest(sys.argv[sys.argv.index("-single") + 1], is_nonrandom_mutation, True, int(max_attack_num))
     elif ("-batch" in sys.argv):
-        filepath = os.path.normpath(os.path.join(os.getcwd(), "../../../vattacker_data/" + sys.argv[sys.argv.index("-batch") + 1]))
-        batch_test = BatchTest(filepath, is_nonrandom_mutation, int(max_attack_num))
+        filepath = os.path.normpath(os.path.join(os.getcwd(), "../../../vattacker_data/"))
+        batch_test = BatchTest(filepath, sys.argv[sys.argv.index("-batch") + 1], is_nonrandom_mutation, int(max_attack_num))
