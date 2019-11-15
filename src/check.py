232e6152ee732eda155691d50a1b02d383ac2d1f
vemakereporter <vemakereporter@gmail.com>
2019-11-15 20:32:13 +0800

    debug

--- src/check.py
+++ src/check.py
@@ -3,9 +3,7 @@ import os
 from Eval.ManualChecker import *
 
 if __name__ == '__main__':
-    input_filepath = os.path.normpath(os.path.join(os.getcwd() + "../../../vattacker_data/result/",
-                                                   sys.argv[sys.argv.index("-resid") + 1]))
+    input_filepath = os.path.normpath(os.path.join(os.getcwd(), "../../../vattacker_data/result/")) \
+                     + sys.argv[sys.argv.index("-resid") + 1]
     output_filepath = input_filepath
     manual_checker = ManualChecker(input_filepath, output_filepath)
-
-
