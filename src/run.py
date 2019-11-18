678d464f4a3941b8f52d643f701d63a9367f08c3
vemakereporter <vemakereporter@gmail.com>
2019-11-18 17:27:00 +0800

    debug

--- src/run.py
+++ src/run.py
@@ -6,14 +6,24 @@ if __name__ == '__main__':
     is_nonrandom_mutation = True
     max_attack_num = 5
     tool = None
-    if ("-tool" in sys.argv):
+    text = None
+    test_id = None
+    if "-tool" in sys.argv:
         tool = sys.argv[sys.argv.index("-tool") + 1]
-    if ("-max" in sys.argv):
+    if "-text" in sys.argv:
+        text = sys.argv[sys.argv.index("-text") + 1]
+    if "-textid" in sys.argv:
+        test_id = sys.argv[sys.argv.index("-testid") + 1]
+    if "-max" in sys.argv:
         max_attack_num = sys.argv[sys.argv.index("-max") + 1]
-    if ("-random" in sys.argv):
+    if "-random" in sys.argv:
         is_nonrandom_mutation = False
-    if ("-single" in sys.argv):
-        single_test = SingleTest(tool, sys.argv[sys.argv.index("-single") + 1], is_nonrandom_mutation, True, int(max_attack_num))
-    elif ("-batch" in sys.argv):
+    if "-single" in sys.argv:
+        print(tool)
+        print(text)
+        print(is_nonrandom_mutation)
+        print(max_attack_num)
+        single_test = SingleTest(tool, text, is_nonrandom_mutation, True, int(max_attack_num))
+    elif "-batch" in sys.argv:
         filepath = os.path.normpath(os.path.join(os.getcwd(), "../../../vattacker_data/"))
-        batch_test = BatchTest(tool, filepath, sys.argv[sys.argv.index("-batch") + 1], is_nonrandom_mutation, int(max_attack_num))
+        batch_test = BatchTest(tool, filepath, test_id, is_nonrandom_mutation, int(max_attack_num))
